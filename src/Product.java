cd6743eaca59d13ba5eb7fdb1ce738ccadf3a557
G00423339@atu.ie <yashshirwant10@gmail.com>
2025-11-03 10:19:18 +0000

    ProductClass

---
+++ src/Product.java
@@ -0,0 +1,16 @@
+//Yash Shirwant
+
+
+package atu.ie
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Product {
+public static void main (String [] args){
+    Scanner scan1 = new Scanner (System.in);
+    ArrayList<Product> products = new ArrayList<Product>();
+    System.out.println("How many products do you want to buy?");
+    int.count = sc.next int()
+    }
+}
---
+++ src/ProductDets.java
@@ -0,0 +1,6 @@
+package atu.ie
+
+
+
+public class ProductDets (String ProductID, String Price, String Quantity )
+this.ProductID = ProductID;
